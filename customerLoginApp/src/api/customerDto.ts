79852aa0fdd527186c699fdc8298d834ca28ee1e
afnesss <af.nesss07@gmail.com>
2026-05-19 10:21:41 +0200

    feat: add customer data types

---
+++ customerLoginApp/src/api/customerDto.ts
@@ -0,0 +1,44 @@
+export type CustomerAgreement = {
+  agreement_id: string;
+  agreement_value: number;
+};
+
+export type CustomerAddress = {
+  address1: string;
+  address2: string;
+  address3: string;
+  address4: string;
+  address5: string;
+  address6: string;
+  address7: string;
+  zip: string;
+  city: string;
+  country_code: string;
+};
+
+export type CustomerConsent = {
+  agreement_gtc: number;
+  agreement_profiling: number;
+  agreement_marketing_communication: number;
+  custom_agreements: CustomerAgreement[];
+};
+
+export type CustomerPersonalInformation = {
+  gender: number;
+  first_name: string;
+  last_name: string;
+  birthdate: string;
+  email: string;
+  phone: string;
+  language_id: string;
+  store_id: string;
+  address: CustomerAddress;
+  agreement: CustomerConsent;
+};
+
+export type User = {
+  customer_id: string;
+  personal_information: CustomerPersonalInformation;
+  last_change: string;
+  state: number;
+};
